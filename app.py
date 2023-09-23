9f5bd1905781193e82ba92300ffe587de9b3fe42
Georges <92797658+georgrmat@users.noreply.github.com>
2023-09-24 00:04:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,23 +10,16 @@ df.rename(columns = {"open_time": "date"}, inplace = True)
 df["date"] = pd.to_datetime(df["date"], unit = "ms")
 df.set_index("date", inplace=True)
 
-
-
 # Define the Streamlit app
 st.title("Candlestick Chart App")
 
-# Add a date range slider to customize the data displayed
-date_range = st.date_input("Select Date Range", min_value=df.index.min(), max_value=df.index.max())
-
-# Filter the data based on the selected date range
-filtered_df = df[(df.index >= date_range[0]) & (df.index <= date_range[1])]
 st.dataframe(filtered_df)
 # Create a candlestick chart using Plotly
-candlestick_chart = go.Figure(data=[go.Candlestick(x=filtered_df.index,
-                                                   open=filtered_df['open'],
-                                                   high=filtered_df['high'],
-                                                   low=filtered_df['low'],
-                                                   close=filtered_df['close'])])
+candlestick_chart = go.Figure(data=[go.Candlestick(x=df.index,
+                                                   open=df['open'],
+                                                   high=df['high'],
+                                                   low=df['low'],
+                                                   close=df['close'])])
 
 # Customize the chart layout
 candlestick_chart.update_layout(title="OHLCV Candlestick Chart",
