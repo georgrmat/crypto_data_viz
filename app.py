f17e6551ef34bd2d123ec268a32db5181f99ba5a
Georges <92797658+georgrmat@users.noreply.github.com>
2023-09-23 23:54:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import pandas as pd
 
 # Load the CSV data
 df = pd.read_csv("df.csv")
+st.dataframe(df)
+
 df["date"] = pd.to_datetime(df["date"])
 df.set_index("date", inplace=True)
-st.dataframe(df)
+
 """
 # Define the Streamlit app
 st.title("Candlestick Chart App")
